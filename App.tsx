1cba4fb9b65430f86202a03e2b581adad01a7608
Erick Giraldo <erickgiraldo94@gmail.com>
2024-11-15 15:16:05 -0500

    feat: Created login and register view. No service yet

--- App.tsx
+++ App.tsx
@@ -8,11 +8,13 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 // import {DemoScreen} from './src/screens/demo.screen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {AnotherScreen, DemoScreen} from './src/screens';
+// import {DemoScreen} from './src/screens';
 import {RootStackParamList} from './src/interfaces/navigation.interface';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {theme} from './src/theme/main.theme';
 import {SyncProvider} from './src/contexts/contacts-syncronization.context';
+import {LoginScreen} from './src/screens/login.screen';
+import {RegistrationScreen} from './src/screens/register.screen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -31,7 +33,7 @@ function App(): React.JSX.Element {
       <SyncProvider>
         <NavigationContainer>
           <Stack.Navigator
-            initialRouteName="Demo"
+            initialRouteName="Register"
             screenOptions={{
               headerStyle: {
                 backgroundColor: theme.colors.background,
@@ -44,11 +46,16 @@ function App(): React.JSX.Element {
               animation: 'slide_from_right',
               freezeOnBlur: true,
             }}>
-            <Stack.Screen name="Demo" component={DemoScreen} />
+            {/* <Stack.Screen name="Demo" component={DemoScreen} /> */}
             <Stack.Screen
-              name="AnotherScreen"
-              component={AnotherScreen}
-              options={{title: 'Another screen'}}
+              name="Register"
+              component={RegistrationScreen}
+              options={{title: 'User registration'}}
+            />
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{title: 'User login'}}
             />
           </Stack.Navigator>
         </NavigationContainer>
--- src/interfaces/navigation.interface.ts
+++ src/interfaces/navigation.interface.ts
@@ -3,4 +3,13 @@ import {ParamListBase} from '@react-navigation/native';
 export interface RootStackParamList extends ParamListBase {
   Demo: undefined;
   AnotherScreen: undefined;
+  Register: undefined;
+  Login: undefined;
+  Cars: undefined;
+  CarDetails: {carId: string};
+  AddCar: undefined;
+  EditCar: {
+    contactId: number;
+  };
+  CarMaintenance: {carId: string};
 }
